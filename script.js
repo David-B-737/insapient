63e4f2ea606a94542606d0d6910f31a66b51c46b
andreygalchevski <andreygalchevski@gmail.com>
2018-04-16 00:40:24 +0300

    added a bunch of stuff: full width carousel, songs section, band section. found out there is a problem with make embedded videos responsive.

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ var sideNavElem = document.querySelector('.sidenav')
 var sideNavInstance = M.Sidenav.init(sideNavElem, options)
 
 var carouselElem = document.querySelector('.carousel')
-var carouselInstance = M.Carousel.init(carouselElem, options)
+var carouselInstance = M.Carousel.init(carouselElem, { fullWidth: true })
