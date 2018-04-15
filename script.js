6e2834940fcd0b8734610b440d6ad18d48fd6389
Andrey Galchevski <andreygalchevski@gmail.com>
2018-04-15 20:54:58 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var options = {}
+
+var sideNavElem = document.querySelector('.sidenav')
+var sideNavInstance = M.Sidenav.init(sideNavElem, options)
+
+var carouselElem = document.querySelector('.carousel')
+var carouselInstance = M.Carousel.init(carouselElem, options)
