b71da690448d7a3c6e6ea2a59b68a88f0d74dfcf
Andrey Galchevski <andreygalchevski@gmail.com>
2018-04-16 07:44:03 +0300

    morning bus ride commit

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ var sideNavInstance = M.Sidenav.init(sideNavElem, options)
 
 var carouselElem = document.querySelector('.carousel')
 var carouselInstance = M.Carousel.init(carouselElem, { fullWidth: true })
+
+autoplay()
+
+function autoplay() {
+    carouselInstance.next()
+    setTimeout(autoplay, 4500)
+}
