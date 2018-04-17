a39ade4b6bd48d71df6ebfeb02fbef7e3474f290
Andrey Galchevski <andreygalchevski@gmail.com>
2018-04-17 15:16:05 +0300

    scrolly

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ var sideNavInstance = M.Sidenav.init(sideNavElem, options)
 var carouselElem = document.querySelector('.carousel')
 var carouselInstance = M.Carousel.init(carouselElem, { fullWidth: true })
 
+var scrollspyElem = document.querySelector('.scrollspy')
+var scrollspyInstance = M.ScrollSpy.init(scrollspyElem, options)
+
 autoplay()
 
 function autoplay() {
