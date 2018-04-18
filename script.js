b453a1770fce41b5b15cbe7836d8f273a20ef692
Andrey Galchevski <andreygalchevski@gmail.com>
2018-04-18 09:37:41 +0300

    long and unproductive bus ride

--- script.js
+++ script.js
@@ -10,8 +10,13 @@ var scrollspyElem = document.querySelector('.scrollspy')
 var scrollspyInstance = M.ScrollSpy.init(scrollspyElem, options)
 
 autoplay()
-
 function autoplay() {
     carouselInstance.next()
     setTimeout(autoplay, 4500)
+}
+
+function closeSideNav () {
+    if (sideNavInstance.isOpen) {
+        sideNavInstance.close()
+    }
 }
